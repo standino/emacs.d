408f62c3e1a9836a1e4514dfe9089d6ad74123c7
Standino <changwei.cn@gmail.com>
2013-12-12 08:09:26 +0000

    org and org page are ready to use

---
+++ lisp/init-moz.el
@@ -0,0 +1,24 @@
+(autoload 'moz-minor-mode "moz" "Mozilla Minor and Inferior Mozilla Modes" t)
+
+(defun moz-custom-setup ()
+  (moz-minor-mode 1)
+  ;; @see  http://www.emacswiki.org/emacs/MozRepl
+  ;; Example - you may want to add hooks for your own modes.
+  ;; I also add this to python-mode when doing django development.
+  (auto-reload-firefox-on-after-save-hook)
+  )
+
+(add-hook 'js2-mode-hook 'moz-custom-setup)
+(add-hook 'html-mode-hook 'moz-custom-setup)
+(add-hook 'nxml-mode-hook 'moz-custom-setup)
+(add-hook 'web-mode-hook 'moz-custom-setup)
+
+(defun auto-reload-firefox-on-after-save-hook ()
+  (add-hook 'after-save-hook
+            '(lambda ()
+               (interactive)
+               (comint-send-string (inferior-moz-process)
+                                   "setTimeout(BrowserReload(), '500');"))
+            'append 'local)) ;; buffer-local
+
+(provide 'init-moz)
