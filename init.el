7bfed55222d56c50b374b9804302c10ebedb78a9
Will <Will@localhost.localdomain>
2014-02-25 11:25:09 +0800

    done by st shell

--- init.el
+++ init.el
@@ -126,7 +126,7 @@
 ;;----------------------------------------------------------------------------
 ;; Locales (setting them earlier in this file doesn't work in X)
 ;;----------------------------------------------------------------------------
-(require 'init-locales)
+;;(require 'init-locales)
 
 (add-hook 'after-init-hook
           (lambda ()
